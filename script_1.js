b148f9cd3448759b589c07b93539f9c7becc6867
Silvia <silviawu1001@gmail.com>
2025-01-16 15:57:15 +0000

    20250115

--- script_1.js
+++ script_1.js
@@ -1,18 +1,12 @@
-    function scrollGallery(direction) {
-        const gallery = document.querySelector('.photo-gallery');
-        const scrollAmount = 300; // Adjust this value as needed
+function scrollGallery(direction) {
+    const gallery = document.querySelector('.photo-gallery');
+    const scrollAmount = 200; // Adjust the amount of scroll per click
+    const currentScroll = gallery.scrollLeft;
 
-        if (direction === 'left') {
-            gallery.scrollBy({
-                left: -scrollAmount,
-                behavior: 'smooth'
-            });
-        } else if (direction === 'right') {
-            gallery.scrollBy({
-                left: scrollAmount,
-                behavior: 'smooth'
-            });
-        }
+    if (direction === 'left') {
+        gallery.scrollLeft = currentScroll - scrollAmount;
+    } else if (direction === 'right') {
+        gallery.scrollLeft = currentScroll + scrollAmount;
     }
-
+}
 
