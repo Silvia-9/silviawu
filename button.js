6c1db61d71ba12cfaaa26a9af06edeb4dcff88b5
Silvia <silviawu1001@gmail.com>
2025-04-05 22:29:15 +0100

    20250405

--- button.js
+++ button.js
@@ -1,6 +1,6 @@
 function toggleContent() {
-    var moreText = document.querySelector('.more-text');
-    var btn = document.querySelector('.read-more-btn');
+    const moreText = document.querySelector('.more-text');
+    const btn = document.querySelector('.read-more-btn');
   
     if (moreText.style.display === "none" || moreText.style.display === "") {
       moreText.style.display = "block";
